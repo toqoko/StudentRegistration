2dfeed6d68c22b1a2088462c658071b0f383c55d
toqoko <sherbakovdanik92@gmail.com>
2024-05-07 14:09:22 +0300

    draft

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const phoneInputs = document.querySelectorAll('input[type="tel"]');
-  phoneInputs.forEach(function(input) {
-      input.addEventListener("input", function() {
-          let value = this.value.replace(/\D/g, '');
-          if (value.length > 0) {
-              value = value.match(/(\d{0,2})(\d{0,3})(\d{0,2})(\d{0,2})/);
-              this.value = !value[2] ? value[1] : value[1] + ' ' + value[2] + (value[3] ? '-' + value[3] : '');
-          }
-      });
-  });
-});
-
 function toggleFields(parentId) {
   const selectedValue = document.getElementById(parentId).querySelector('select').value;
 
